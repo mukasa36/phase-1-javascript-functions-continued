2bf56af3510ce7d7342ef44d53d902c85cc7dade
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-06-20 00:16:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+
+function saturdayFun(mypar = "roller-skate") {
+    return "This Saturday, I want to " + mypar + "!";
+  }
+
+  function mondayWork(myparr = "go to the office") {
+
+    return "This Monday, I will " + myparr + ".";
+  }
+  function wrapAdjective(myparrr) {
+
+    return function (myDefault = "Special") {
+
+      if (myparrr =="*") {  
+
+        return `You are ${myparrr}${myDefault}${myparrr}!`;
+
+      } else if (myparrr == "||") {
+
+        return `You are ${myparrr}${myDefault}${myparrr}!`;
+      }
+
+    };
+    
+  }
+  
+  
